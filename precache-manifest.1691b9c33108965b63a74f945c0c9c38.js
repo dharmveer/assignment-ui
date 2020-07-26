9486b63f2001ae172162b630311a5ad37a900c7f
Dharmveer Kumar <dharm187@in.ibm.com>
2020-07-26 17:58:25 +0530

    PWA-adding

---
+++ precache-manifest.1691b9c33108965b63a74f945c0c9c38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df31086a990ed60504c7",
+    "url": "/assignment-ui/css/app.d40fc157.css"
+  },
+  {
+    "revision": "746459cc0bc66b9c4a13db0a99d7c9b3",
+    "url": "/assignment-ui/index.html"
+  },
+  {
+    "revision": "df31086a990ed60504c7",
+    "url": "/assignment-ui/js/app.635d7ea2.js"
+  },
+  {
+    "revision": "eea52ed1a340fc4d170b",
+    "url": "/assignment-ui/js/chunk-vendors.8d5d6769.js"
+  },
+  {
+    "revision": "d6d2f2b0d8eae9b5b157fa214047f421",
+    "url": "/assignment-ui/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/assignment-ui/robots.txt"
+  }
+]);
